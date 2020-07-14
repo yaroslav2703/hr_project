a98edcb062c24a03c0c75a63824ab0f97e1b40bf
yarvald <yaroslav.pitsukha@gmail.com>
2020-07-14 17:02:29 +0300

    part 2

--- frontend/src/views/Vacancies.vue
+++ frontend/src/views/Vacancies.vue
@@ -1,6 +1,75 @@
 <template>
     <div>
-        <h1>ШУЕ</h1>
+
+        <div class="row">
+            <div class="col s3 m3 l3">
+                <div class="card blue-grey lighten-5">
+                    <div class="card-content white-text">
+                        <a class="btn-floating btn-large waves-effect waves-light orange darken-2"><i class="material-icons">add</i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s3 m3 l3">
+                <div class="card blue-grey lighten-5">
+                    <a class="black-text">
+                        <div class="card-content ">
+                            <span class="card-title">Программист</span>
+                            <p>опыт: Больше 1 лет</p>
+                            <p>Уровень: Junior</p>
+                            <p>Отдел: Разработка </p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col s3 m3 l3">
+                <div class="card orange darken-2">
+                    <a class="white-text">
+                        <div class="card-content ">
+                            <span class="card-title">Программист</span>
+                            <p>опыт: Больше 1 лет</p>
+                            <p>Уровень: Junior</p>
+                            <p>Отдел: Разработка </p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col s3 m3 l3">
+                <div class="card orange darken-2">
+                    <a class="white-text">
+                        <div class="card-content ">
+                            <span class="card-title">Программист</span>
+                            <p>опыт: Больше 1 лет</p>
+                            <p>Уровень: Junior</p>
+                            <p>Отдел: Разработка </p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col s3 m3 l3">
+                <div class="card orange darken-2">
+                    <a class="white-text">
+                        <div class="card-content ">
+                            <span class="card-title">Программист</span>
+                            <p>опыт: Больше 1 лет</p>
+                            <p>Уровень: Junior</p>
+                            <p>Отдел: Разработка </p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col s3 m3 l3">
+                <div class="card orange darken-2">
+                    <a class="white-text">
+                        <div class="card-content ">
+                            <span class="card-title">Программист</span>
+                            <p>опыт: Больше 1 лет</p>
+                            <p>Уровень: Junior</p>
+                            <p>Отдел: Разработка </p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
