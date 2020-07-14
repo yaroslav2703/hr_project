ad407c641ac01e1d4fed5121afde3cc0f9a17504
yarvald <yaroslav.pitsukha@gmail.com>
2020-07-14 13:51:08 +0300

    part 2

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <component :is="layout">
+
+    </component>
+  </div>
+</template>
+
+<script>
+  import MainLayout from "./layouts/MainLayout";
+
+export default {
+  name: 'App',
+  computed: {
+    layout() {
+      return 'main-layout'
+    }
+  },
+  components: {
+    MainLayout
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '~materialize-css/dist/css/materialize.min.css';
+  @import 'assets/index.css';
+</style>
